8017961aa2d4c963f350b2edb42e0776835af064
FatherOfCurses <colin.principe@gmail.com>
2025-09-24 21:16:51 -0400

    Add client Prompt handling

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,10 @@
-import { input, select } from "@inquirer/prompts";
+import "dotenv/config"
+import {confirm, input, select} from "@inquirer/prompts";
 import { Client } from "@modelcontextprotocol/sdk/client";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
-import { Tool } from "@modelcontextprotocol/sdk/types.js";
-import { describe } from "node:test";
+import { Prompt, PromptMessage, Tool } from "@modelcontextprotocol/sdk/types.js";
+import { generateText } from "ai";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
 
 const mcp = new Client({
     name: "test-client",
@@ -17,6 +19,10 @@ const transport = new StdioClientTransport({
     command: "node",
     args: ["./build/server.js"],
     stderr: "ignore" // ignore errors because we're using experimental node features
+})
+
+const google = createGoogleGenerativeAI({
+    apiKey: process.env.GEMINI_API_KEY
 })
 
 async function main() {
@@ -80,6 +86,24 @@ async function main() {
                 }
                 break
             }
+
+            case "Prompts": {
+                const promptName = await select({
+                    message: "Select a prompt",
+                    choices: prompts.map(prompt => ({      // get list of prompts and display
+                        name: prompt.name,
+                        value: prompt.name,
+                        description: prompt.description
+                    })),
+                })
+                const prompt = prompts.find(prompt => prompt.name === promptName)
+                if (prompt == null) {
+                    console.error("Prompt not found")
+                } else {
+                    await handlePrompt(prompt)
+                }
+                break
+            }
         }
     }
 }
@@ -109,7 +133,7 @@ async function handleResource(uri: string) {
     for (const paramMatch of paramMatches) {
         const paramName = paramMatch.replace("{", "").replace("}", "")
         const paramValue = await input({
-            message: `Enter value for ${paramName.slice(1, -1)}:`
+            message: `Enter value for ${paramName}:`
         })
         finalUri = finalUri.replace(paramMatch, paramValue)
     }
@@ -122,6 +146,44 @@ async function handleResource(uri: string) {
     console.log(
         JSON.stringify(JSON.parse(res.contents[0].text as string), null, 2)
     )
+}
+
+async function handlePrompt(prompt: Prompt) {
+    const args: Record<string, string> = {}
+    for (const arg of prompt.arguments ?? []) {
+        args[arg.name] = await input({
+            message: `Enter value for ${arg.name}:`
+            // looping through the params defined on the server and asking for input
+        })
+    }
+
+    const response = await mcp.getPrompt({
+        name:prompt.name,
+        arguments: args
+    })  // parse out what was obtained from the args above
+
+    for (const message of response.messages) {
+        console.log(await handleServerMessagePrompt(message))
+    }
+}
+
+async function handleServerMessagePrompt(message: PromptMessage) {
+    if (message.content.type !== "text") return
+
+    console.log(message.content.text)
+    const run = await confirm({   // from inquirer - simple yes/no prompt
+        message: "Do you want to run this?",
+        default: true
+    })
+
+    if (!run) return
+
+    const { text } = await generateText({
+        model: google("gemini-2.0-flash"),
+        prompt: message.content.text,
+    })
+
+    return text
 }
 
 main()
